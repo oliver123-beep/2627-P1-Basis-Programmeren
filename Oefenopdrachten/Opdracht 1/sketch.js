2f6af2d3d5d405ed19b833dc3868d3a5e1ce7b7e
oliver123-beep <olivierdewitte025@gmail.com>
2026-09-07 11:55:23 +0200

    oefenopdracht 1

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
+  strokeWeight(5)
   background(220);
+  fill(R=255, G=0, B=0) 
+  rect(100, 100, 100, 100)
+  fill(R=0, G=255, B=0)
+  rect(200, 150, 200, 100)
+  fill(R=0, G=0, B=255)
+  rect(100, 200, 100, 200)
+  fill(R=255, G=255, B=255)
+  rect(0, 100, 100, 200)
+  fill(R=0, G=255, B=0)
+  rect(0, 300, 100, 100)
+  fill(R=0, G=0, B=255)
+  rect(0, 0, 200, 100)
+  fill(R=255, G=255, B=255)
+  rect(200, 0, 200, 200)
+  fill(R=255, G=0, B=0)
+  rect(200, 300, 200, 100)
+  fill(R=255, G=255, B=255)
+  rect(200, 250, 100, 50)
+  fill(R=0, G=0, B=255)
+  rect(300, 250, 100, 50)
+  fill(R=0, G=0, B=0)
+  rect(400, 200, 100, 200)
+  fill(R=255, G=255, B=255)
+  rect(500, 200, 100, 200)
+  fill(R=255, G=0, B=0)
+  rect(400, 0, 200, 200)
 }
