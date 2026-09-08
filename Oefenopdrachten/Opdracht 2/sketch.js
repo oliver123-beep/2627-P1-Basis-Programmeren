19ce2fe8cdd8f93fcc821f2834890452e35efc8c
oliver123-beep <olivierdewitte025@gmail.com>
2026-09-08 10:49:01 +0200

    oefenopdracht 1

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -2,6 +2,17 @@ function setup() {
   createCanvas(400, 400);
 }
 
+
+ 
 function draw() {
   background(220);
+  let x = 100  
+  text(x,20,20)
+  let greeting = "hello world"
+  text(greeting,20,60)
+  let a = 20
+  let b = 10
+  let optellen = a + b
+  text("optellen: " + optellen,80,80)
+  
 }
