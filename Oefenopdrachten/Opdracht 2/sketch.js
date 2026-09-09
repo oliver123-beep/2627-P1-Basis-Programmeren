53d90c56ddd4a9ffff8e34cec51c0c49bab8aaf3
oliver123-beep <olivierdewitte025@gmail.com>
2026-09-09 14:29:43 +0200

    opdracht 2

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,18 +1,26 @@
 function setup() {
   createCanvas(400, 400);
 }
-
-
  
 function draw() {
   background(220);
+  //100 wordt een variable
   let x = 100  
   text(x,20,20)
+  //hello world wordt een variable 
   let greeting = "hello world"
   text(greeting,20,60)
+  //20 wordt staat gelijk aan a en 10 aan b
   let a = 20
   let b = 10
+  //rekensommen 
   let optellen = a + b
-  text("optellen: " + optellen,80,80)
-  
+  let aftrekken = a - b
+  let vermenigvuldigen = a * b
+  let delen = a / b
+  //de text
+  text("optellen: " + optellen,20,80)
+  text("aftrekken: " + aftrekken,20,100)
+  text("vermenigvuldigen: " + vermenigvuldigen,20,120)
+  text("delen: " + delen,20,140)
 }
